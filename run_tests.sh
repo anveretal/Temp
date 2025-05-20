e7aad14bfa10cbdd89dd875f47fb08cc08f0bd7c
AnVeA <AnVeA_1@mail.ru>
2025-05-20 18:46:01 +0700

    edit run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
-export LD_LIBRARY_PATH=~/Desktop/OSs/NSU-OSs/install:~/Desktop/OSs/NSU-OSs/install/plugins:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=~/Desktop/Temp/install:~/Desktop/Temp/install/plugins:$LD_LIBRARY_PATH
 ./tests/run_tests.py --src . --lab 1
